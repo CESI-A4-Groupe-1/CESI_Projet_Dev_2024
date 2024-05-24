9c39196a69330ee1977c395a69443a9a7ab80a94
PantinEcarlate <tristanloup.koch@viacesi.fr>
2024-05-24 14:50:21 +0200

    clean du projet et ajout d'un simple titre dans App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import {useSlots} from "vue";
 </script>
 
 
@@ -20,45 +17,14 @@ export default {
 }
 </script>
 
-<template>
-  <header v-on:click="uneFonction()"> <!--ajout de lignes inutiles ici -->
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
-    <router-view/>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!"/>
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome/>
-  </main>
+
+
+<template>
+  <h1>CESI eats</h1>
+
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
