bd63a2c56cb5f5e08a1f088bf324ca752a6f7401
lilouRBL <lilou.rabouille@gmail.com>
2024-06-05 11:34:32 +0200

    add categories unstyled

---
+++ frontend/CEeats/src/components/CategoryCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="single_category">
+    <p class="category_name">Catégorie 1</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .single_category {
+    background-color: lightgrey;
+    width: 45%;
+    height: 100px;
+    display: flex;
+    position: relative;
+    justify-content: flex-end;
+    align-items: flex-end;
+    padding: 10px;
+    .category_name {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      margin: 10px;
+    }
+  }
+</style>
---
+++ frontend/CEeats/src/components/CategoryRow.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import SingleCategory from "@/components/CategoryCard.vue";
+</script>
+
+<template>
+  <div class="category_row">
+    <SingleCategory v-for="(category, i) in 2" :key="i"/>
+  </div>
+</template>
+
+<style scoped>
+.category_row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+
+</style>
