55708a6e7515900b1b3a106793f087ca24cfcb14
lilouRBL <lilou.rabouille@gmail.com>
2024-06-05 10:31:05 +0200

    add customer view

---
+++ frontend/CEeats/src/views/customer/CustomerView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <input class="search"></input>
+    <h2 class="topCategories">Top Categories</h2>
+    <p>component</p>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
