b443ad0090e0fdc33c50f8052a7dc96f8ae4dd5b
PantinEcarlate <tristanloup.koch@viacesi.fr>
2024-06-05 15:50:49 +0200

    Renommage d'une vue pour le nom correct : la vue qui affiche les catégories de restaurants disponibles (par tags).
    Ajout de plusieurs routes (NON FONCTIONNELLES) dans le fichier du routeur

--- frontend/CEeats/src/router/index.ts
+++ frontend/CEeats/src/router/index.ts
@@ -1,21 +1,67 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import MainView from '@/views/MainView.vue'
-import CustomerView from '@/views/customer/CustomerView.vue'
+import TagView from '@/views/customer/TagsView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'test',
-      component: MainView
-    },
-    {
-      path: '/parcourir',
-      name: 'parcourir',
-      component: CustomerView
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'test',
+            component: MainView
+        },
+        {
+            path: '/parcourir',
+            name: 'parcourir',
+            component: TagView
+        },
+        {
+            path: '/users',
+            name: 'users',
+            component: MainView, //TODO : changer le composant pour que ce soit la liste des utilisateurs accessible uniquemnet par les administrateurs
+            children: [
+                {
+                    path: '/:id/orders',
+                    name: 'users_orders',
+                    component: MainView,
+                    beforeEnter: (to, from, next): void => {
+                        let isAdmin: boolean = false;
+                        if (isAdmin) {
+                            return next(to)
+                        }
+                    }
+                },
+                {
+                    path: '/:id',
+                    component: MainView, //TODO : changer le composant pour que ce soit la vue d'un utilisateur unique. Utiliser la variable "isAdmin" pour valider ou invalider la requête
+                    beforeEnter: (to, from, next) => {
+                        let isAdmin: boolean = false;
+                        if (isAdmin) {
+                            return next(to)
+                        }
+                    }
+                }
+            ]
+        },
+        {
+            path: '/articles',
+            component: MainView, //TODO : changer le composant vers la vue de tout les articles
+            name: 'articles',
+            children: [
+                {
+                    path: '/:id',
+                    name: 'specific_article',
+                    component: MainView,
+                    beforeEnter: (to, from, next) => {
+                        let isAllowed: boolean = false;
+                        if (isAllowed) {
+                            return next(to)
+                        }
+                    }
+                }
+            ]
+        }
+    ]
 })
 
 export default router
