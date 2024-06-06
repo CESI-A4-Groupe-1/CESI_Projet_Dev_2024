1171b68c9d513f2cab2fc6060387c7751368aecb
Arslane Fares Ould Slimane <arslane02@gmail.com>
2024-06-06 09:12:03 +0200

    Add passing authentication headers to upstream service headers (#9)
    * Basic API Authentication
    
    * Renommage d'une vue pour le nom correct : la vue qui affiche les catégories de restaurants disponibles (par tags).
    
    Ajout de plusieurs routes (NON FONCTIONNELLES) dans le fichier du routeur
    
    * Ajout de deux fonctions dans le routeur.
    
    Ces deux fonctions serviront à
    - authentifier l'utilisateur lambda pour accéder à des pages réservées au utilisateurs authentifiés
    - attester des permissions d'un utilisateur administrateur
    
    * Pass User Infos Headers to services
    
    ---------
    
    Co-authored-by: PantinEcarlate <tristanloup.koch@viacesi.fr>

--- frontend/CEeats/src/router/index.ts
+++ frontend/CEeats/src/router/index.ts
@@ -1,15 +1,77 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import MainView from '@/views/MainView.vue'
-import CustomerView from '@/views/customer/CustomerView.vue'
-import ResultsView from "@/views/customer/ResultsView.vue";
+import TagView from '@/views/customer/TagsView.vue'
+
+//TODO : ici la méthode qui sera utilisée pour faire une requête au serveur d'authentification afin de vérifier la connexion de l'utilisateur
+function authenticateUser(): boolean {
+    return false;
+}
+
+//TODO : ici la méthode qui sera utilisée pour faire une requête au serveur d'authentification afin de vérifier les permissions de l'utilisateurs
+function authenticateAdmin(): boolean {
+    return false;
+}
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-      { path: '/', name: 'test', component: MainView },
-      { path: '/parcourir', name: 'parcourir', component: CustomerView },
-      { path: '/resultats', name: 'resultats', component: ResultsView },
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'test',
+            component: MainView
+        },
+        {
+            path: '/parcourir',
+            name: 'parcourir',
+            component: TagView
+        },
+        {
+            path: '/users',
+            name: 'users',
+            component: MainView, //TODO : changer le composant pour que ce soit la liste des utilisateurs accessible uniquemnet par les administrateurs
+            children: [
+                {
+                    path: '/:id/orders',
+                    name: 'users_orders',
+                    component: MainView,
+                    beforeEnter: (to, from, next): void => {
+                        let isAdmin: boolean = authenticateAdmin();
+                        if (isAdmin) {
+                            return next(to)
+                        }
+                    }
+                },
+                {
+                    path: '/:id',
+                    component: MainView, //TODO : changer le composant pour que ce soit la vue d'un utilisateur unique. Utiliser la variable "isAdmin" pour valider ou invalider la requête
+                    beforeEnter: (to, from, next) => {
+                        let isAdmin: boolean = authenticateAdmin();
+                        if (isAdmin) {
+                            return next(to)
+                        }
+                    }
+                }
+            ]
+        },
+        {
+            path: '/articles',
+            component: MainView, //TODO : changer le composant vers la vue de tout les articles
+            name: 'articles',
+            children: [
+                {
+                    path: '/:id',
+                    name: 'specific_article',
+                    component: MainView,
+                    beforeEnter: (to, from, next) => {
+                        let isAllowed: boolean = authenticateUser();
+                        if (isAllowed) {
+                            return next(to)
+                        }
+                    }
+                }
+            ]
+        }
+    ]
 })
 
 export default router
