ce4dbae5cb7b1cdcda728cdb603b6bd3a1c8593c
lilouRBL <lilou.rabouille@gmail.com>
2024-06-06 09:07:37 +0200

    refactore name CustomerView

--- frontend/CEeats/src/router/index.ts
+++ frontend/CEeats/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '@/views/MainView.vue'
-import CustomerView from '@/views/customer/CustomerView.vue'
+import ExploreView from '@/views/customer/ExploreView.vue'
 import ResultsView from "@/views/customer/ResultsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
       { path: '/', name: 'test', component: MainView },
-      { path: '/parcourir', name: 'parcourir', component: CustomerView },
+      { path: '/parcourir', name: 'parcourir', component: ExploreView },
       { path: '/resultats', name: 'resultats', component: ResultsView },
   ]
 })
