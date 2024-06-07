336aa6c5892b263caa3e2cdc1412e5153d665023
PantinEcarlate <87308542+PantinEcarlate@users.noreply.github.com>
2024-06-07 11:34:04 +0200

    Refonte / bugFix / factorFix : "ExploreView" vue des différentes catégories (#11)
    * Changement de la dispositions des catégories. Elles sont maintenant disposées sur la page avec un conteneur flex et alignées avec un flex-wrap.
    
    La vue "ExploreView" est maintenant responsive
    
    La vue "ExploreView" utilise l'API option de vue pour gérer son comportement.
    
    Le composant "CategoryCard" utilise maintenant l'API option pour gérer son comportement
    
    Le composant "CategoryCard" utilise un prop pour récupérer les données envoyé par la vue "ExploreView" afin de gérer lui même les liens au pages qui lui sont associées.
    
    * WarnFix : tag is no longuer redondant.
    
    * BugFix : changement d'une balise "div" vers une balise "routerLink" pour mettre en place les redirections en cas de sélection.
    
    ---------
    
    Co-authored-by: PantinEcarlate <tristanloup.koch@viacesi.fr>

--- frontend/CEeats/src/components/CategoryCard.vue
+++ frontend/CEeats/src/components/CategoryCard.vue
@@ -1,56 +1,69 @@
 <script setup lang="ts">
 
 </script>
+<script>
 
+export default {
+  data() {
+
+  },
+  props: [
+    "tagName"
+  ]
+}
+
+</script>
 <template>
-  <div class="single_category">
-    <p class="middle">Catégorie 1</p>
-    <p class="category_name">Catégorie 1</p>
-  </div>
+  <RouterLink to="/categories" class="single_category">
+    <p class="middle">{{ tagName }}</p>
+    <p class="category_name">{{ tagName }}</p>
+  </RouterLink>
+
 </template>
 
 <style lang="scss" scoped>
-  .single_category {
-    background-color: lightgrey;
-    width: 45%;
-    height: 100px;
-    display: flex;
-    position: relative;
-    justify-content: flex-end;
-    align-items: flex-end;
-    padding: 10px;
-    margin-bottom: 20px;
-    .category_name {
-      position: absolute;
-      bottom: 0;
-      right: 0;
-      margin: 10px;
-    }
-  }
+.single_category {
+  background-color: lightgrey;
+  width: 45%;
+  height: 100px;
+  display: flex;
+  position: relative;
+  justify-content: flex-end;
+  align-items: flex-end;
+  padding: 10px;
+  margin-bottom: 20px;
 
-  .middle {
-    transition: .5s ease;
-    opacity: 0;
+  .category_name {
     position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    text-align: center;
+    bottom: 0;
+    right: 0;
+    margin: 10px;
   }
+}
 
-  .single_category:hover {
-    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
-  }
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
 
-  .single_category:hover .middle {
-    opacity: 1;
-    font-size: larger;
-    margin: 0;
-  }
+.single_category:hover {
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+}
 
-  .single_category:hover .category_name {
-    display: none;
-  }
+.single_category:hover .middle {
+  opacity: 1;
+  font-size: larger;
+  margin: 0;
+}
+
+.single_category:hover .category_name {
+  display: none;
+}
 
 </style>
