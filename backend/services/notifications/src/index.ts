4eff263b674f14d3274c165724d4cd38ae692637
Arslane Fares Ould Slimane <arslane02@gmail.com>
2024-06-17 15:08:18 +0200

    Implémentation du service des notifications (#22)
    * Implement Notifications
    
    * update gitignore
    
    * frontend .env

--- backend/services/notifications/src/index.ts
+++ backend/services/notifications/src/index.ts
@@ -1,10 +1,14 @@
-import express from "express"
+// src/index.ts
+import express from 'express';
+import bodyParser from 'body-parser';
+import notificationRoutes from './routes/notifications';
 
 const app = express();
-app.listen(3006, () => {
-    console.log(`Server running on port 3006`)
-})
 
-app.get("/", (req: any, res: any) => {
-    res.send("Hello Notifications, Hello World!")
-})
+app.use(bodyParser.json());
+app.use('/notifications', notificationRoutes);
+
+const PORT = 3006;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
