5b51a67f8b35943695b7d2abe1213e29f3b38288
ElMarchand <91123329+ElMarchand@users.noreply.github.com>
2024-06-17 10:02:47 +0200

    Elo (#19)
    * dockerize database
    
    * Database Configuration
    
    * install sequelize
    
    * Sequelize Authentication
    
    * dockerize database
    
    * Database Configuration
    
    * install sequelize
    
    * Sequelize Authentication
    
    * Implement User controller and refactor
    
    * Fix isAdmin
    
    * Finished User CRUD
    
    * dump
    
    * fix db
    
    * Ajout du service Commandes et Livraisons
    
    ---------
    
    Co-authored-by: ArslaneFOS <arslane@yieldy.fr>
    Co-authored-by: lilouRBL <98310199+lilouRBL@users.noreply.github.com>

--- backend/services/orders_deliveries/src/index.ts
+++ backend/services/orders_deliveries/src/index.ts
@@ -1,11 +1,18 @@
+require("dotenv").config()
 import express from "express"
+import routes from "./routes/order_deliveries.routes"
 
 const app = express();
+app.use(express.json())
+
+app.use(express.urlencoded({extended: false}))
+
 app.listen(3001, () => {
     console.log(`Server running on port 3001`)
 })
 
+routes(app)
+
 app.get("/", (req: any, res: any) => {
-    console.log(req.headers)
-    res.status(200).json({msg: "Welcome to the Orders Service"})
+    res.send("Welcome to the Orders and Deliveries Service")
 })
---
+++ backend/services/orders_deliveries/src/models/articles.ts
@@ -0,0 +1,98 @@
+import * as Sequelize from 'sequelize';
+import { DataTypes, Model, Optional } from 'sequelize';
+import type { CommandeList, CommandeListId } from './commande_list';
+import type { Section, SectionId } from './section';
+
+export interface ArticlesAttributes {
+  id: number;
+  nom: string;
+  prix: number;
+  id_section: number;
+  description?: string;
+}
+
+export type ArticlesPk = "id";
+export type ArticlesId = Articles[ArticlesPk];
+export type ArticlesOptionalAttributes = "id" | "description";
+export type ArticlesCreationAttributes = Optional<ArticlesAttributes, ArticlesOptionalAttributes>;
+
+export class Articles extends Model<ArticlesAttributes, ArticlesCreationAttributes> implements ArticlesAttributes {
+  id!: number;
+  nom!: string;
+  prix!: number;
+  id_section!: number;
+  description?: string;
+
+  // Articles hasMany CommandeList via id_article
+  commande_lists!: CommandeList[];
+  getCommande_lists!: Sequelize.HasManyGetAssociationsMixin<CommandeList>;
+  setCommande_lists!: Sequelize.HasManySetAssociationsMixin<CommandeList, CommandeListId>;
+  addCommande_list!: Sequelize.HasManyAddAssociationMixin<CommandeList, CommandeListId>;
+  addCommande_lists!: Sequelize.HasManyAddAssociationsMixin<CommandeList, CommandeListId>;
+  createCommande_list!: Sequelize.HasManyCreateAssociationMixin<CommandeList>;
+  removeCommande_list!: Sequelize.HasManyRemoveAssociationMixin<CommandeList, CommandeListId>;
+  removeCommande_lists!: Sequelize.HasManyRemoveAssociationsMixin<CommandeList, CommandeListId>;
+  hasCommande_list!: Sequelize.HasManyHasAssociationMixin<CommandeList, CommandeListId>;
+  hasCommande_lists!: Sequelize.HasManyHasAssociationsMixin<CommandeList, CommandeListId>;
+  countCommande_lists!: Sequelize.HasManyCountAssociationsMixin;
+  // Articles belongsTo Section via id_section
+  id_section_section!: Section;
+  getId_section_section!: Sequelize.BelongsToGetAssociationMixin<Section>;
+  setId_section_section!: Sequelize.BelongsToSetAssociationMixin<Section, SectionId>;
+  createId_section_section!: Sequelize.BelongsToCreateAssociationMixin<Section>;
+
+  static initModel(sequelize: Sequelize.Sequelize): typeof Articles {
+    return sequelize.define('Articles', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      field: 'ID'
+    },
+    nom: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      field: 'Nom'
+    },
+    prix: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    id_section: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'section',
+        key: 'ID'
+      },
+      field: 'ID_section'
+    },
+    description: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      field: 'Description'
+    }
+  }, {
+    tableName: 'articles',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "ID" },
+        ]
+      },
+      {
+        name: "ID_section",
+        using: "BTREE",
+        fields: [
+          { name: "ID_section" },
+        ]
+      },
+    ]
+  }) as typeof Articles;
+  }
+}
