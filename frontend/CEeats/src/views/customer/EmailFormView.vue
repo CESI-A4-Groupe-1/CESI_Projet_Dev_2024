2cb478301c9a3ec14374ea33c5adfcebfebe9ade
lilouRBL <lilou.rabouille@gmail.com>
2024-06-17 13:43:19 +0200

    Implementation des fonctionnalités de modification de l'utilisateur

--- frontend/CEeats/src/views/customer/EmailFormView.vue
+++ frontend/CEeats/src/views/customer/EmailFormView.vue
@@ -1,22 +1,71 @@
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent, onMounted, ref} from 'vue'
+import {useRoute} from "vue-router";
+import User from '@/models/UserModel';
+import {AccountService} from "@/services";
 
 export default defineComponent({
-  name: "EmailFormView"
+  name: "EmailFormView",
+  setup() {
+    const route = useRoute();
+    const userId = ref<string>(route.params.id as string);
+    // Variables réactives
+    const user = ref<User>({
+      id: '',
+      nom: '',
+      prenom: '',
+      email: '',
+      role_id: '',
+      date_anniv: '',
+      password: '',
+      telephone: '',
+      is_notified: false,
+      path_pfp: '',
+      id_parain: '',
+      adresse: '',
+      createdAt: '',
+      updatedAt: '',
+      deletedAt: ''
+    });
+    onMounted(() => {
+      AccountService.getUser(userId.value)
+          .then(res => {
+            user.value = res.data as User;
+          })
+          .catch(err => console.log(err));
+    });
+
+    const updateUser = async () => {
+      try {
+        await AccountService.updateUser(userId.value, user.value);
+        alert("Utilisateur mis à jour avec succès");
+      } catch (err) {
+        console.error(err);
+        alert("Erreur lors de la mise à jour de l'utilisateur");
+      }
+    };
+
+    // Retour des variables et fonctions à utiliser dans le template
+    return {
+      user,
+      updateUser
+    };
+  }
 })
 </script>
 
 <template>
   <main>
     <Button icon="pi pi-arrow-left" severity="secondary" text rounded aria-label="Bookmark" @click="$router.go(-1)"></Button>
-
-    <h1>Adresse e-mail</h1>
-    <p>Utilisez cette adresse e-mail vous connecter et récupérer votre compte.</p>
-    <div class="form">
-      <label for="email">E-mail</label>
-      <InputText id="email" v-model="email"/>
-    </div>
-    <Button label="Mettre à jour" raised />
+    <form @submit.prevent="updateUser">
+      <h1>Adresse e-mail</h1>
+      <p>Utilisez cette adresse e-mail vous connecter et récupérer votre compte.</p>
+      <div class="form">
+        <label for="email">E-mail</label>
+        <InputText id="email" v-model="user.email" :value="user.email"/>
+      </div>
+      <Button label="Mettre à jour" type="submit" raised />
+    </form>
   </main>
 </template>
 
--- frontend/CEeats/src/views/customer/PhoneFormView.vue
+++ frontend/CEeats/src/views/customer/PhoneFormView.vue
@@ -1,23 +1,73 @@
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent, onMounted, ref} from 'vue'
+import {useRoute} from "vue-router";
+import {AccountService} from "@/services";
+import User from '@/models/UserModel';
+
 
 export default defineComponent({
-  name: "PhoneFormView"
+  name: "PhoneFormView",
+  setup() {
+    const route = useRoute();
+    const userId = ref<string>(route.params.id as string);
+    // Variables réactives
+    const user = ref<User>({
+      id: '',
+      nom: '',
+      prenom: '',
+      email: '',
+      role_id: '',
+      date_anniv: '',
+      password: '',
+      telephone: '',
+      is_notified: false,
+      path_pfp: '',
+      id_parain: '',
+      adresse: '',
+      createdAt: '',
+      updatedAt: '',
+      deletedAt: ''
+    });
+    onMounted(() => {
+      AccountService.getUser(userId.value)
+          .then(res => {
+            user.value = res.data as User;
+          })
+          .catch(err => console.log(err));
+    });
+
+    const updateUser = async () => {
+      try {
+        await AccountService.updateUser(userId.value, user.value);
+        alert("Utilisateur mis à jour avec succès");
+      } catch (err) {
+        console.error(err);
+        alert("Erreur lors de la mise à jour de l'utilisateur");
+      }
+    };
+
+    // Retour des variables et fonctions à utiliser dans le template
+    return {
+      user,
+      updateUser
+    };
+  }
 })
 </script>
 
 <template>
 <main>
   <Button icon="pi pi-arrow-left" severity="secondary" text rounded aria-label="Bookmark" @click="$router.go(-1)"></Button>
+  <form @submit.prevent="updateUser">
+    <h1>Numéro de téléphone</h1>
+    <p>Utilisez ce numéro pour vous connecter et récupérer votre compte.</p>
 
-  <h1>Numéro de téléphone</h1>
-  <p>Utilisez ce numéro pour vous connecter et récupérer votre compte.</p>
-
-  <div class="form">
-    <label for="phone">Numéro de téléphone</label>
-    <InputText id="phone" v-model="phone"/>
-  </div>
-  <Button label="Mettre à jour" raised />
+    <div class="form">
+      <label for="phone">Numéro de téléphone</label>
+      <InputText id="phone" v-model="user.telephone" :value="user.telephone"/>
+    </div>
+    <Button label="Mettre à jour" type="submit" raised />
+  </form>
 </main>
 </template>
 
