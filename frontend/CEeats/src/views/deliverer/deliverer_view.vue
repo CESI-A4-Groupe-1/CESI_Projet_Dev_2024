acb0a0aab6fff4a0d95ff89affa07440434db241
PantinEcarlate <tristanloup.koch@viacesi.fr>
2024-06-18 11:26:27 +0200

    - structure principale de la vue du livreur
    -ajout des routes nécessaire à l'apparition de cette vue

--- frontend/CEeats/src/views/deliverer/deliverer_view.vue
+++ frontend/CEeats/src/views/deliverer/deliverer_view.vue
@@ -1,15 +1,53 @@
-<script lang="ts">
+<script lang="js">
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: "deliverer_view"
+  name: "deliverer_view",
+  data() {
+    let delivery_data;
+    let delivery_id;
+    return {
+      delivery_data, //les données de livraison sont stockées ici. Cette variable peut muter en objet si le besion s'en fait ressentir
+      delivery_id,
+    }
+  },
+  async mounted() {
+    //TODO : préparer l'url pour récupérer les données de livraison ici
+    this.delivery_id = $router.params.id
+
+    this.delivery_data = await (await fetch("/api/delivery/" + this.delivery_id)).json()
+
+    //TODO : ajouter des étapes de traitement supplémentaires selon les données renvoyées par la requête fetch
+  }
 })
 </script>
 
-<template>
 
+<template>
+  <div class="container">
+    <p v-if="delivery_data != null">{{ delivery_data }}</p>
+    <p v-else class="error">Aucune adresse n'a été fournie pour cette url</p>
+  </div>
 </template>
 
-<style scoped>
 
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: normal;
+  border: 4px solid black;
+  border-radius: 10px;
+  padding: 5px;
+  font-size: 18px;
+}
+
+.error {
+  background-color: #DF1717;
+  color: white;
+  font-size: 18px;
+}
 </style>
