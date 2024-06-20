a7cbdcbf6a1c01be0396bc0ddff4d3b2965eaf68
lilouRBL <lilou.rabouille@gmail.com>
2024-06-20 17:11:31 +0200

    Mise a jour vite.config

--- frontend/CEeats/vite.config.ts
+++ frontend/CEeats/vite.config.ts
@@ -2,11 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {https:true},
   plugins: [
+    mkcert(),
     vue(),
+    VitePWA({
+      strategies: 'injectManifest',
+      injectRegister: null,
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+        type: 'module',
+        navigateFallback: 'index.html'
+      },
+      workbox: {
+        sourcemap: true
+      }
+    })
   ],
   resolve: {
     alias: {
